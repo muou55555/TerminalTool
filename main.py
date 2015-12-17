78fd6dd1ab64e4d6bbaf94727808a9569e89e7b9
muou <zhangxw@bhz.com.cn>
2015-12-17 12:40:02 +0800

    modify menu

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ __email__ = 'muou55555@163.com'
 __url__ = 'https://github.com/muou55555/TerminalTool'
 __description__ = __doc__
 
+from helper.pylog import Pylog
 from view.pymenu import Pymenu
+from application.apptelnet import AppTelnet
 
 def f_quit():
     c = raw_input("Quit? y or n :")
@@ -22,26 +24,36 @@ def f_quit():
         
 def fun_test():
     print "fun_test() Run..."
+    t = AppTelnet("192.168.1.46")
+    t.test()
     
 def fun(s):
     print s      
      
 if __name__ == '__main__':
-     main_menu = Pymenu("Main Menu", [20, 20, 20], back_key = "q")
-     main_menu.add_field("1", "Telnet", lambda:fun("Telnet"))
-     main_menu.add_field("2", "Ftp", lambda:fun("Ftp"))
+     #logging.basicConfig(level=logging.DEBUG, format='%(module)s %(levelname)s: %(message)s')
+     log = Pylog(level=2)   
+    
+     main_menu = Pymenu("Main Menu", [20, 30, 30], back_key = "q")
+     
+     telnet_menu = Pymenu("Telnet Menu", [20, 30, 30],  back_key = "0")
+     telnet_menu.add_field("1", "Connect",  lambda:AppTelnet().connect())
+     telnet_menu.add_field("2", "Close", lambda:AppTelnet().close()) 
+     telnet_menu.add_field("3", "Run One Commond", lambda:AppTelnet().run_cmd()) 
+     telnet_menu.add_field("4", "Run Commond", lambda:AppTelnet().running_cmd()) 
+     telnet_menu.add_field("5", "Run Script", lambda:AppTelnet().run_pys())
+     telnet_menu.add_field("6", "Check Rtc", lambda:AppTelnet().check_rtc())  
+     telnet_menu.add_field("7", "Kill App", lambda:AppTelnet().kill_process("concentrator")) 
+     telnet_menu.add_field("8", "Device Status", lambda:AppTelnet().device_status()) 
+     telnet_menu.add_field("9", "Init DB", lambda:AppTelnet().init_db()) 
+     telnet_menu.add_field("0", "Back", lambda:fun("Back"))
+     
+     main_menu.add_field("1", "Telnet", telnet_menu.show)
+     main_menu.add_field("2", "Ftp", lambda:AppTelnet().run_cmd("date\n"))
      main_menu.add_field("3", "Serial", lambda:fun("Serial"))
      main_menu.add_field("4", "SSH", fun_test)
      main_menu.add_field("5", "Mbus", lambda:fun("Mbus"))
      main_menu.add_field("6", "Settings", lambda:fun("Settings"))
-     
-     sub_menu = Pymenu("SubMenu", [20, 20, 20],  back_key = "0")
-     sub_menu.add_field("a", "sub a", lambda:fun("sub a"))
-     sub_menu.add_field("b", "sub b", None) 
-     sub_menu.add_field("c", "sub c", None) 
-     sub_menu.add_field("0", "Back", lambda:fun("Back"))
-     
-     main_menu.add_field("s", "Submenu", sub_menu.show)
      main_menu.add_field("q", "Quit", f_quit)
 
      main_menu.show()
